0762bc552d0a328eba3664e7e68fc7827e1c8730
Anjani B M <anjani.b-m@dxc.com>
2024-02-15 11:40:34 +0530

    Add project files.

---
+++ Day4Training/CalculatorPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Day4Training
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculatorPage : ContentPage
+    {
+        public CalculatorPage()
+        {
+            InitializeComponent();
+        }
+/*
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(Num1.Text);
+            int num2 =  Convert.ToInt32(Num2.Text);
+            int c = num1+ num2;
+            result.Text = c.ToString();
+
+        }
+        */
+    }
+}
